a4874985fb8d6a92d4d7f692c4f3eccbecb38119
Harsh Agnihotri <harshagnihotri90@gmail.com>
2023-01-17 03:22:57 +0530

    almost done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Nav from "./Components/home/nav";
 import Edu_certi from "./Components/skills/Quali_comp";
 import Projects from "./Components/projects/Projects";
 import About from "./Components/about/About";
+import Portfolio from "./Components/Portfolio/portfolio";
+import Contact from "./Components/contact/Contact";
 
 function App() {
 
@@ -19,6 +21,9 @@ function App() {
       <About />
       <Edu_certi />
       <Projects />
+      <Portfolio/>
+      <Contact/>
+
       <script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script> 
     </div>
   );

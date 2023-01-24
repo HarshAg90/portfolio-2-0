c01ab89860ed687111a444d87cde3d7fc3d3fab8
Harsh Agnihotri <harshagnihotri90@gmail.com>
2023-01-24 17:16:11 +0530

    DONE

--- src/Components/about/About.js
+++ src/Components/about/About.js
@@ -4,8 +4,8 @@ import "./about.css";
 export default function About() {
   return (
     <div id="about">
-      <h1 className="Title">About Me - </h1>
-      <p>
+      <h1 className="Title" data-aos="fade-down">About Me - </h1>
+      <p data-aos="zoom-in" data-aos-delay="200">
         I am a Computer Science Student and Software developer
         <br />I like to work on frontend styling and writting Scripts
         <br />
@@ -15,16 +15,16 @@ export default function About() {
         <br />
         and trying to make a career in big facinating tech world
       </p>
-      <div className="edu">
+      <div className="edu" data-aos="fade-down">
         <h1>Education</h1>
-        <div className="schools">
+        <div className="schools" data-aos="fade-down">
           <h2>Bachlor of Computer Academics</h2>
           <h3>
             <span>MSI IPU - Delhi, India</span> <span>2021-2024</span>
           </h3>
         </div>
       </div>
-      <p>
+      <p data-aos="zoom-in" data-aos-delay="200">
         I have many leadership and management skills
         <br /> I am Currently Web Developemt department head In GDSC MSIT
         <br /> I have won many Debates & competitions, I have been college Class
